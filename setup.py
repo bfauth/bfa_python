264fd0484965e870d13eac2bd88e8f2d8fbe13ed
ceigh <hilth@protonmail.com>
2018-12-09 22:28:04 +0500

    fix(setup.py): Fix wrong classifiers

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
